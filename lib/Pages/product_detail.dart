ddd8020e1ad514a3bcd06d18210fc8e10757787d
IT <sayyambutt317@gmail.com>
2024-02-28 05:27:38 +0500

    product ui updating

--- lib/Pages/product_detail.dart
+++ lib/Pages/product_detail.dart
@@ -1,15 +1,14 @@
 import 'dart:ui';
-
 import 'package:flutter/material.dart';
 
 class ProductDetail extends StatefulWidget {
   const ProductDetail({
-    super.key,
+    Key? key,
     required this.productName,
     required this.productPrice,
     required this.productImage,
     required this.productDescription,
-  });
+  }) : super(key: key);
 
   final String productName;
   final String productPrice;
@@ -25,52 +24,86 @@ class _ProductDetailState extends State<ProductDetail> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: const EdgeInsets.all(10),
-          child: Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              BackdropFilter(
-                filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
-                child: Container(
-                  width: double.infinity,
-                  height: MediaQuery.of(context).size.height * 0.4,
-
-                  color: Colors.grey.withOpacity(0.1),
-                  child: Center(
-                    child: Image.network(widget.productImage),
-                  ),
+        body: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            BackdropFilter(
+              filter: ImageFilter.blur(sigmaX: 10.0, sigmaY: 10.0),
+              child: Container(
+                width: double.infinity,
+                height: MediaQuery.of(context).size.height * 0.4,
+                color: Colors.white70,
+                child: Center(
+                  child: Image.network(widget.productImage),
                 ),
               ),
-              Container(
-                height: MediaQuery.of(context).size.height*0.529,
-                width: double.infinity,
-                decoration: BoxDecoration(
-                    borderRadius:const BorderRadius.only(
+            ),
+            Column(
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.54,
+                  width: double.infinity,
+
+                  decoration: BoxDecoration(
+                    color: Colors.white.withOpacity(0.4),
+                    borderRadius: const BorderRadius.only(
                       topLeft: Radius.circular(40),
                       topRight: Radius.circular(40),
                     ),
-                  border: Border.all(
-                    width: 3,
-                    color: Colors.green,
-                    style: BorderStyle.solid,
+                    border: Border.all(
+                      width: 1,
+                      color: Colors.black.withOpacity(0.2),
+                      style: BorderStyle.solid,
+                    ),
                   ),
-                ),
-                child: Padding(
-                  padding: const EdgeInsets.all(15.0),
-                  child: Text(
-                    widget.productName,
-                    style: const TextStyle(
-                      fontSize: 22,fontWeight: FontWeight.bold
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          widget.productName,
+                          style: const TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Padding(
+                              padding: const EdgeInsets.all(10.0),
+                              child: Text(
+                                '\$ ${widget.productPrice} ',
+                                style: const TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.w600,
+                                ),
+                              ),
+
+                            ),
+
+                          ],
+                        ),
+                        const Text(
+                          "Description",
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        Text(
+                          widget.productDescription,style:  const TextStyle(color: Colors.grey,fontSize: 10),
+                        ),
+                        const Text("Choose Size")
+                      ],
                     ),
                   ),
                 ),
-
-              )
-
-           ],
+              ],
+            ),
+          ],
         ),
-      ),
       ),
     );
   }
