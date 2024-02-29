1dae78695e6efea03980dc52ff56c9210ac5ede3
Sayyam Butt <sayyambutt317@gmail.com>
2024-02-29 17:51:16 +0500

    text

---
+++ lib/Helper/Color/colors.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class AppColor {
+  AppColor._();
+
+  //App Basic Colors
+  static const Color primaryColor = Color(0xFF4b68ff);
+  static const Color secondaryColor = Color(0xFFFFE24B);
+  static const Color accentColor = Color(0xFFb0c7ff);
+
+  //App Text Colors
+  static const Color primaryTextColor = Color(0xFF333333);
+  static const Color secondaryTextColor = Color(0xFF6C7570);
+  static const Color extWhite = Colors.white;
+
+  //App Background Colors
+  static const Color lightColor = Color(0xFFF6F6F6);
+  static const Color darkColor = Color(0xFF272727);
+  static const Color primaryBackgroundColor = Color(0xFFF3F5FF);
+
+  //Background Container Color
+  static const Color lightBackgroundColor = Color(0xFFF6F6F6);
+  static const Color darkBackgroundColor = Color(0xFF272727);
+
+  //Button Colors
+  static const Color primaryButtonColor = Color(0xFF4b68ff);
+  static const Color secondaryButtonColor = Color(0xFF6C7570);
+  static const Color accountDisabled = Color(0xFFC4C4C4);
+
+  //Border Colors
+  static const Color primaryBorderColor = Color(0xFFD9D9D9);
+  static const Color secondaryBorderColor = Color(0xFFE6E6E6);
+
+  //Error and Validation Color
+  static const Color errorColor = Color(0xFFD32F2F);
+  static const Color successColor = Color(0xFF388E3C);
+  static const Color warningColor = Color(0xFFF57C00);
+  static const Color infoColor = Color(0xFF197602);
+
+  //Neutral shade
+  static const Color black = Color(0xFF232323);
+  static const Color darkerGrey = Color(0xFF4F4F4F);
+  static const Color darkGrey = Color(0xFF939393);
+  static const Color grey = Color(0xFFEEEEEE);
+  static const Color lightGrey = Color(0xFFFAFAFA);
+  static const Color softGrey = Color(0xFFF4F4F4);
+  static const Color white = Color(0xFFFFFFFF);
+}
---
+++ lib/Helper/Sizes/TextSize.dart
@@ -0,0 +1,38 @@
+class Size {
+  //padding and margin sizes
+  static const double xs = 4.0;
+  static const double sm = 8.0;
+  static const double md = 16.0;
+  static const double lg = 24.0;
+  static const double xl = 32.0;
+
+  //Icon sizes
+  static const double iconXs = 12.0;
+  static const double iconSm = 16.0;
+  static const double iconMd = 24.0;
+  static const double iconLg = 32.0;
+  static const double iconXl = 48.0;
+
+//Font Sizes
+  static const double fontSizeSm = 14.0;
+  static const double fontSizeMd = 16.0;
+  static const double fontSizeLg = 18.0;
+
+  //ButtonSizes
+
+  static const double btnHeight = 18.0;
+  static const double btnRadius = 12.0;
+  static const double btnWidth = 120.0;
+  static const double btnElevation = 4.0;
+
+  //Appbar Height
+  static const double appBarHeight = 56.0;
+
+  //Images size
+  static const double imgThumbSize = 80.0;
+
+  //Default spacing between sections
+  static const double defaultSpacing = 24.0;
+  static const double spacebtwItems = 16.0;
+  static const double spacebtwSections = 32.0;
+}
