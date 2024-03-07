059e530c023393e68fdb94d65f0f306c70c0c37b
IT <sayyambutt317@gmail.com>
2024-03-07 05:26:36 +0500

    cart and appbar

---
+++ lib/common/Cart/cart_icon.dart
@@ -0,0 +1,46 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:iconsax/iconsax.dart';
+
+import '../../../../Helper/Color/colors.dart';
+
+class CartCounterIcon extends StatelessWidget {
+  const CartCounterIcon({
+    super.key,  required this.onPressed, required this.iconColor,
+  });
+
+  final VoidCallback onPressed;
+  final Color iconColor;
+  @override
+  Widget build(BuildContext context) {
+    return Stack(
+      children: [
+        IconButton(
+            onPressed: () {},
+            icon: const Icon(Iconsax.shopping_bag,
+                color: AppColor.white)),
+        Positioned(
+            right: 0,
+            child: Container(
+              width: 18,
+              height: 18,
+              decoration: BoxDecoration(
+                color: AppColor.black,
+                borderRadius: BorderRadius.circular(100),
+              ),
+              child: Center(
+                child: Text(
+                  '2',
+                  style: Theme.of(context)
+                      .textTheme
+                      .labelLarge!
+                      .apply(
+                      color: AppColor.white,
+                      fontSizeFactor: 0.8),
+                ),
+              ),
+            ))
+      ],
+    );
+  }
+}
