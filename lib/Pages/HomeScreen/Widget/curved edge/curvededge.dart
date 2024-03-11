2f9b76fa8e0847a774a01137d1754c1a232f63a1
Sayyam Butt <sayyambutt317@gmail.com>
2024-03-11 10:49:00 +0500

    unnecessary import

--- lib/Pages/HomeScreen/Widget/curved edge/curvededge.dart
+++ lib/Pages/HomeScreen/Widget/curved edge/curvededge.dart
@@ -2,35 +2,32 @@ import 'package:flutter/cupertino.dart';
 
 class CustomCurvedEdges extends CustomClipper<Path> {
   @override
-Path getClip(Size size){
-  var path =Path();
-  path.lineTo(0,size.height);
+  Path getClip(Size size) {
+    var path = Path();
+    path.lineTo(0, size.height);
 
-  final firstCurve = Offset(0,size.height-20);
-  final lastCurve = Offset(30,size.height-20);
-  path.quadraticBezierTo(firstCurve.dx, firstCurve.dy, lastCurve.dx, lastCurve.dy);
+    final firstCurve = Offset(0, size.height - 20);
+    final lastCurve = Offset(30, size.height - 20);
+    path.quadraticBezierTo(
+        firstCurve.dx, firstCurve.dy, lastCurve.dx, lastCurve.dy);
 
-  final secondFirstCurve = Offset(0,size.height-20);
-  final secondLastCurve = Offset(size.width-30,size.height-20);
-  path.quadraticBezierTo(secondFirstCurve.dx, secondFirstCurve.dy, secondLastCurve.dx, secondLastCurve.dy);
+    final secondFirstCurve = Offset(0, size.height - 20);
+    final secondLastCurve = Offset(size.width - 30, size.height - 20);
+    path.quadraticBezierTo(secondFirstCurve.dx, secondFirstCurve.dy,
+        secondLastCurve.dx, secondLastCurve.dy);
 
+    final lastFirstCurve = Offset(size.width, size.height - 20);
+    final lastSecondCurve = Offset(size.width, size.height);
+    path.quadraticBezierTo(lastFirstCurve.dx, lastFirstCurve.dy,
+        lastSecondCurve.dx, lastSecondCurve.dy);
 
-  final lastFirstCurve = Offset(size.width,size.height-20);
-  final lastSecondCurve = Offset(size.width,size.height);
-  path.quadraticBezierTo(lastFirstCurve.dx, lastFirstCurve.dy, lastSecondCurve.dx, lastSecondCurve.dy);
+    path.lineTo(size.width, 0);
+    path.close();
+    return path;
+  }
 
-  path.lineTo(size.width,0);
-  path.close();
-  return path;
-
-}
   @override
   bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
-    // TODO: implement shouldReclip
     throw UnimplementedError();
-
-
   }
-
-  }
-
+}
